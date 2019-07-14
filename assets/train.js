7c94790af1a4d8ca78f2d1effa32930842435df2
Colin McMahon <cmcmahon1012@gmail.com>
2019-07-14 12:58:55 -0400

    started the js, added the on-click function for entering a new train into the scedule

---
+++ assets/train.js
@@ -0,0 +1,46 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDuNwQAlXGlg6uhBme4omi4YqW5kfnQTmI",
+    authDomain: "train-schedule-b56a1.firebaseapp.com",
+    databaseURL: "https://train-schedule-b56a1.firebaseio.com",
+    projectId: "train-schedule-b56a1",
+    storageBucket: "",
+    messagingSenderId: "841111604226",
+    appId: "1:841111604226:web:1e4f71d5b4cf5c73"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  $("#add-train-btn").on("click", function(){
+      event.preventDefault();
+
+      var trainName = $("#train-namer").val();
+      var destName = $("#destination-namer").val();
+      var trainTime = $("#train-timer").val();
+      var frequencyTime = $("#frequence").val();
+
+      var trainEntry = {
+          name: trainName,
+          dest: destName,
+          time: trainTime,
+          freq: frequencyTime
+      };
+
+      database.ref().push(trainEntry);
+
+      console.log(trainEntry.name);
+      console.log(trainEntry.dest);
+      console.log(trainEntry.time);
+      console.log(trainEntry.freq);
+
+      $("#train-namer").val("");
+      $("#destination-namer").val("");
+      $("#train-timer").val("");
+      $("#frequence").val("");
+  })
+
+  
+
